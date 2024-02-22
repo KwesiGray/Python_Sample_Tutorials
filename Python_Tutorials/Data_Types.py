383f328fc75016cffbb18e571ae35a8d802fb948
Agent Breslin <ce-dggraham9721@st.umat.edu.gh>
2024-02-22 02:42:33 +0000

    Python_Tutorials

---
+++ Python_Tutorials/Data_Types.py
@@ -0,0 +1,70 @@
+# ALL THE DATA TYPES IN JAVA YOU SHOULD KNOW.
+
+print("WELCOME TO PYTHON DATA TYPES!!")
+print("")
+
+
+# A string 
+s = str("Hello World")	#str
+	
+# An Integer Number
+i = int(20)	#int	
+
+# A Floating Number
+f = float(20.5)	#float
+
+# A Complex Number / with the letter J at the end
+c = complex(1j)	#complex
+	
+# A List / Separated with just commas
+l = list(("rock", "sand", "water"))	#list	
+
+# A Tuple / Separated with just commas
+t = tuple(("grapes", "strawberry", "cherry"))	#tuple
+
+# A Range starting withe the number 6	
+r = range(6)	#range	
+
+# A Dictionary / Giving Variables values in the same bracket is how its implemented
+d = dict(name="John", age=36)	#dict	
+
+s = set(("red", "yellow", "black"))	#set	
+
+g = frozenset(("dog", "cat", "lion"))	#frozenset	
+
+b = bool(5)	#bool	
+
+n = bytes(5)	#bytes
+	
+z = bytearray(5)	#bytearray	
+
+m = memoryview(bytes(3))	#memo
+
+
+print("")
+print("Printing them out to see how their outputs looks like..")
+
+print(s)
+print(i)
+print(f)
+print(c)
+print(l)
+
+#pay attention to the output of the List datatype ,Tuple , & sets
+print("pay attention to the output of the List datatype ,Tuple , & set")
+print("")
+
+#for the List
+print("or the List")
+print(l)
+
+
+#for the Tuple
+print("for the Tuple")
+print(t)
+
+
+#for the sets
+print("for the Sets")
+print(s)
+
