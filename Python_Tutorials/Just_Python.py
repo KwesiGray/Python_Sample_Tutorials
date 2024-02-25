50c6f30c6d5e430c4096124784977cf47c465432
Agent Breslin <ce-dggraham9721@st.umat.edu.gh>
2024-02-25 18:52:46 +0000

    Python_Tutorials

--- Python_Tutorials/Just_Python.py
+++ Python_Tutorials/Just_Python.py
@@ -8,3 +8,26 @@ print(str(jay)+ " is the random number printed")
 
 g= 3+5j + 4+3j
 print(g)
+
+print("")
+
+# How to print Paragraphs in Python.
+print("How to print Paragraphs in Python.")
+print("")
+
+Par='''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+'''
+print(Par)
+
+#Printing Chracters from a String using their Index.
+print("")
+print("Printing Chracters from a String using their Index.")
+
+a = "Human Being"
+print(a + " Is the String Under Review")
+
+print("Printing From index 0-5 of the word HUMAN BEING ")
+print("The New String is: "+ a[0:5])
