b8e1e27ce04a0c522313f14994a3364f1419109b
Agent Breslin <ce-dggraham9721@st.umat.edu.gh>
2024-03-12 23:11:12 +0000

    Boolean Expresions

---
+++ Booleans.py
@@ -0,0 +1,7 @@
+# In programming you often need to know if an expression is True or False.
+# You can evaluate any expression in Python, and get one of two answers, True or False.
+
+# A Simple Boolean Representation 
+print(10 > 9) #Prints True 
+print(10 == 9) # PRINTS False
+print(10 < 9) # Prints False
