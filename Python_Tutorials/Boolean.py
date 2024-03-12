9383dae1e8accad826fefa7bb61b879dae02d276
Agent Breslin <ce-dggraham9721@st.umat.edu.gh>
2024-03-12 23:26:19 +0000

    Booleans

--- Python_Tutorials/Boolean.py
+++ Python_Tutorials/Boolean.py
@@ -5,3 +5,21 @@
 print(10 > 9) #Prints True 
 print(10 == 9) # PRINTS False
 print(10 < 9) # Prints False
+print("\n")
+
+# Print a message based on whether the condition is True or False:
+
+print("Using the IF STATEMENTS HERE to check whether a condition is true or not")
+
+a = 34
+b = 13.5
+
+if a>b :
+    print("a is " + str(a))
+    print("b is " + str(b))
+    print("a is greater than b")
+else:
+    print("b is greater or equal to a")
+print("\n")
+
+#Evaluate Variables and variables    
