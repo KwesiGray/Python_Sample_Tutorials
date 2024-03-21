53e514c2c0c79da56670a644017f9ed2a6ffa36c
David Graham <ce-dggraham9721@st.umat.edu.gh>
2024-03-21 23:08:12 +0000

    Functions_Python

---
+++ Python_Tutorials/Recursive_Function.py
@@ -0,0 +1,19 @@
+# A Recursive function in Python using Factorial.
+#learning how to call functions
+def my_function():
+    print("hello")
+
+my_function()
+
+#Passing Arguments to a Function and calling it 
+def names(fname):
+    print(fname + " HI")
+
+names("david")
+names("robot")
+
+#Passing 2 Arguments to a Function and calling it 
+def cars(car,color):
+    print(car, "" ,color)
+
+cars("Benz","Red")
