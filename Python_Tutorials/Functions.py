f9751a783073c7f2c18bae9e7858f0f1ae3d46e6
David Graham <ce-dggraham9721@st.umat.edu.gh>
2024-03-22 07:55:12 +0000

    Functions_In_Py

--- Python_Tutorials/Functions.py
+++ Python_Tutorials/Functions.py
@@ -1,9 +1,18 @@
-# A Recursive function in Python using Factorial.
-#learning how to call functions
+#Learning how to call functions
+"""
+-A function is a block of code which only runs when it is called.
+-You can pass data, known as parameters, into a function.
+-A function can return data as a result.
+"""
+
+
+
 def my_function():
     print("hello")
 
+#To call a function, use the function name followed by parenthesis:
 my_function()
+
 
 #Passing Arguments to a Function and calling it 
 def names(fname):
@@ -11,6 +20,7 @@ def names(fname):
 
 names("david")
 names("robot")
+
 
 #Passing 2 Arguments to a Function and calling it 
 def cars(car,color):
