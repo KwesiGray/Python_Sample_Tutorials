4d59b555ff9adbed32398d501f8c3612284ac74e
David Graham <ce-dggraham9721@st.umat.edu.gh>
2024-03-27 20:23:35 +0000

    Matplotlib_Pyplot

---
+++ Python_Tutorials/Matplot_Lib.py
@@ -0,0 +1,23 @@
+# This file is used to demonstrate the use of Matplotlib library in Python.
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Using the numpy library to generate some data for the graph
+x = np.arange(6, 20+1, 2)
+print(x)
+
+#Having a function to generate the y values for the graph
+y = x**3+x**2-190
+print(y)
+
+#Common Methods used in Matplotlib.Pyplot with regards to plotting a graph and visualizing data.                                                                                                                  
+plt.plot(x, y)
+plt.plot(x, y, 'b--')   #b-- is the color of the line in the graph
+plt.plot(x, y, 'ro')    #ro is the color of the points in the graph
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('Graph of y = x^3+x^2-190')
+plt.grid()
+plt.plot(x, y, marker='o', color='r', linestyle='--', linewidth=2, markersize=6)#This is the same as the above 3 lines of code but this time with the marker size and line width.
+
+plt.show()      #To display the graph graphically.
